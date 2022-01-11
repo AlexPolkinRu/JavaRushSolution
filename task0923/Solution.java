10c4f316d710c4134f978276b1b61e769af1f323
Aleksandr Polochkin <alex.polkin@gmail.com>
2022-01-11 18:48:32 +0300

    Последние 5 задач

---
+++ task0923/Solution.java
@@ -0,0 +1,66 @@
+package Javarush.task0923;
+
+//package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Гласные и согласные
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            String str = reader.readLine();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char character) {
+        character = Character.toLowerCase(character);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+        for (char vowel : vowels) {  // ищем среди массива гласных
+            if (character == vowel) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
